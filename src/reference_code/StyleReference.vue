544676274a8cc1e5cf93a703dfc92b57e46a6e91
hxrxxthh <270051149@yoobeestudent.ac.nz>
2022-04-05 11:18:38 +1200

    Update StyleReference.vue

---
+++ src/reference_code/StyleReference.vue
@@ -0,0 +1,34 @@
+/////////////////////////////
+// *STYLE REFERENCE SHEET *// 
+/////////////////////////////
+
+
+
+//////// FONT FACE //////////
+/* if u want to link */
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+-
+font-family: 'Inter', sans-serif;
+
+/* if you want to import*/
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+</style>
+/////////////////////////////
+
+
+
+////// COLOR STYLES ///////
+#FFFFFF
+/* WHITE Please use for TEXT and ICONS */
+#111127
+/* DARK BLUE */ 
+#63B798
+/* GREEN */ 
+#9369CE
+/* PURPLE */ 
+//////////////////////////////
+
+
