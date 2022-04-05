44b3d6f20c686427b38f1cd859afb7c994d912a8
NatalieCK <270053597@yoobeestudent.ac.nz>
2022-04-05 17:01:36 +1200

    Update ReferenceCode.vue

--- src/reference_code/ReferenceCode.vue
+++ src/reference_code/ReferenceCode.vue
@@ -1,3 +1,5 @@
+
+// BASIC DEFINE PROPS
 <script setup>
 defineProps({
   ApiObject: {
@@ -8,9 +10,9 @@ defineProps({
 });
 </script>
 
-<script>
-import Thingy from "./thingy.vue";
 
+// EXPORT DEFAULT SETUP 
+<script>
 export default {
   data() {
     return {};
@@ -28,3 +30,45 @@ export default {
   beforeCreate: function () {},
 };
 </script>
+
+
+// HAVING SUBCOMPONENT IN PROP SYSTEM 
+<template>
+  <h1>{{ PropSample }}</h1>
+  <!-- ^needs to be sent from its parent component -->
+</template>
+<script setup>
+defineProps({
+  PropSample: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+// IN PARENT COMPONENT 
+<script setup>
+import ChildComponent from "./components/ChildComponent.vue";
+</script>
+<template>
+  <ChildComponent PropSample="Message I sent" />
+  <!-- ^^this will load into subcomponent as a prop -->
+</template>
+
+// HAVING AN ARRAY BASED OFF DATA 
+<template>
+  <ul>
+    <li v-for="'liItems' in myArray”>{{ liItems.title }}</li>
+    <!-- ^ can add more items and it will just keep running -->
+  </ul>
+</template>
+ <script> 
+export default {
+      data() {
+              return{
+                     myArray:[  { title: 'title_1'}, {title: 'title_2'} ]
+               };
+       }
+       methods: {
+       }, 
+}
+ </script> 
