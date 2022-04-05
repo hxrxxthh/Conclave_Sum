0ca75a9f78ee49a345ac90b221b46639e8bf28cc
TheRealTomWilliamson <270153381@yoobeestudent.ac.nz>
2022-04-05 17:07:54 +1200

    anim

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 
 import Avatar from "./components/Avatar.vue";
+import LoadingAnimation from "./components/LoadingAnimation.vue";
 
 </script>
 
 <template>
 <Avatar />
 <router-view></router-view>
+<LoadingAnimation />
 
 </template>
 
